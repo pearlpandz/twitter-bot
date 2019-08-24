d7a518821139daf655cb8273da2723105ad897ef
Muthupandi  V <muthupandi.v@servion.com>
2019-08-24 17:49:45 +0530

    src init commit

---
+++ keys.py
@@ -0,0 +1,11 @@
+'''
+Keys and tokens
+Keys, secret keys and access tokens management.
+'''
+# Consumer API keys
+CONSUMER_KEY = 'pIY3IkJy40MPPlU6QmbGfGVhm'
+CONSUMER_SECRET = '5fJc56Px1TaeOcQa79wPznV7WewQXE04fV6d2eJugLHAmP5rW5'
+
+# Access token & access token secret
+ACCESS_KEY = '1100798967426646016-mVxoClteKN6Zuzl2zDiGXdNmzlrtuW'
+ACCESS_SECRET = 'PGIsQYC2kULXQY81VY5BP4wjOktSOUfDhFLmUYuAleKar'
